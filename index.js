39ee988036a3ee32b3139c8f955f052f04f51b3b
saiderrahmani <didi.mousedidi@outlook.com>
2023-04-06 00:36:48 -0400

    comlete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(num1, num2) {
+  return num1 / num2;
+};
+console.log(divide(10, 2)); // Output: 5
+const square = (num) => {
+  return num * num;
+};
+
+console.log(square(5)); // Output: 25
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(add(2, 3)); // Output: 5
